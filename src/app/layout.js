28111376430bcafe2b80c0244e5f627961669d5d
JimmyCatalogs <jimmyw@catalogs.com>
2025-01-10 11:11:55 -0500

    clerk auth - signup redirect fix

--- src/app/layout.js
+++ src/app/layout.js
@@ -19,7 +19,7 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   return (
-    <ClerkProvider>
+    <ClerkProvider afterSignUpUrl="/">
       <html lang="en">
         <body
           className={`${geistSans.variable} ${geistMono.variable} antialiased`}
